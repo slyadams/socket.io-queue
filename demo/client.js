8ba4ad2d9381d45f0fd223d2aa4d948081397af0
Simon Adams <simonadams@gmail.com>
2014-12-16 16:50:54 +0000

    added buffer and window sizes, cli params to clients, retransmit errors, buffer overflows

--- demo/client.js
+++ demo/client.js
@@ -1,9 +1,27 @@
 var Client = require('../lib/client.js');
-var client = new Client('ws://localhost:8080/nsp');
 
+var cli = require('commander');
+
+cli
+  .version('0.0.1')
+  .option('-u, --url [url]', 'URL to connect to, default="ws://localhost:8080/nsp"')
+  .option('-i, --ignore_retransmit_error [buffer_delay]', 'Whether to ignore retransmit errors, default=0')
+  .option('-d, --process_delay [process_delay]', 'How long to delay processing a message in ms, default=1000')
+  .parse(process.argv);
+
+var url  = cli.url || 'ws://localhost:8080/nsp';
+var process_delay = cli.process_delay || 1000;
+var client = new Client(url);
+if (cli.ignore_retransmit_error) {
+  client.setIgnoreRetransmitErrors(true);
+}
 
 client.on('connect', function() {
   console.log('Connected');
+});
+
+client.on('disconnect', function() {
+  console.log('Disconnected');
 });
 
 var last_sequence = 0;
@@ -14,11 +32,22 @@ client.on('data', function(client_message) {
   setTimeout(function() {
     last_sequence = client_message.getSequence();
     client_message.done();
-  }, 1000);
+  }, process_delay);
 });
 
 client.on('retransmit', function(data) {
   console.log("Will get retransmit from "+data.sequence);
+});
+
+client.on('control', function(data) {
+  console.log("Received control " + data.type + "(" + data.code + ") : " + data.name);
+});
+
+client.on('retransmit_error', function(data, can_continue) {
+  console.log('Retransmit errors '+can_continue);
+  if (!can_continue) {
+    process.exit();
+  }
 });
 
 // setup key handlers to send retransmit requests
@@ -38,4 +67,5 @@ s.on('data', function (letter) {
   if (letter  == '\u0003') {
     process.exit();
   }
+
 });
--- demo/server.js
+++ demo/server.js
@@ -1,8 +1,6 @@
-//todo: clever debugging, tests
-
 // output buffer stats
 function print_buffer_stats (connection, stats) {
-		console.log(connection.getConnectionID() + ' :: Buffer size ' + stats.length + " (" + stats.sent + " sent / "  + stats.unsent + " unsent)");
+		console.log(connection.getConnectionID() + ' :: Buffer size ' + stats.length + "/" + stats.size + " (" + stats.sent + " sent / "  + stats.unsent + " unsent)");
 }
 
 var Connection = require('../lib/connection.js');
@@ -11,22 +9,26 @@ var io = require('socket.io')();
 
 cli
   .version('0.0.1')
-  .option('-p, --port [port]', 'Port')
-  .option('-d, --delay [delay]', 'Data push delay in ms')
-  .option('-b, --buffer_delay [buffer_delay]', 'Buffer display delay in ms, 0=no output')
+  .option('-p, --port [port]', 'Port, default=8080')
+  .option('-d, --delay [delay]', 'Data push delay in ms, default=500')
+  .option('-b, --buffer_delay [buffer_delay]', 'Buffer display delay in ms, 0=no output, default=5000')
+  .option('-w, --window_size [window_size]', 'Window size, default=5')
+  .option('-s, --buffer_size [buffer_size]', 'Max buffer size, default=100')
   .parse(process.argv);
 
 var port  = cli.port || 8080;
 var data_delay = cli.delay || 500;
 var buffer_display_delay = cli.buffer_delay || 5000;
+var window_size = cli.window_size || 5;
+var buffer_size = cli.buffer_size || 100;
 
 var nsp = io.of('/nsp');
 io.listen(port);
 
-// you can pass in a parameer to control how often to push data
+// you can pass in a parameter to control how often to push data
 
 nsp.on('connection', function (socket) {
-	var c = new Connection(socket);
+	var c = new Connection(socket, window_size, buffer_size);
 	var push_data_timer = setInterval(function() { 
 		if (socket.connected) {
 			c.push_data({a: "test"});
